95c451a1055a91dee33a72db56a3944bb9f5bf97
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-02-18 14:10:17 +0200

    feat: spollers

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,7 @@
 
 // import { BaseHelpers } from './helpers/base-helpers';
 
-/**
- * Код попадает в итоговый файл, только когда вызвана функция, например FLSFunctions.spollers();
- * Или когда импортирован весь файл, например import "files/script.js";
- * Неиспользуемый код в итоговый файл не попадает.
-
- * Если мы хотим добавить модуль следует его раскомментировать
- */
-
+import { _slideUp, _slideDown, _slideToggle } from './modules/slideUp';
 import { initCountdown } from './modules/timer-countdown';
 import { addDate } from './modules/addDate';
 import { toTop } from './modules/toTop';
@@ -18,8 +11,13 @@ import { tabs } from './modules/tabs';
 import { headerFon } from './modules/header-fon';
 import { menuBurger } from './modules/menu-burger';
 import { pageNavigation } from './modules/page-navigation';
+import { spollers } from './modules/spollers';
 
-// initCountdown('.countdown', 86440, 'The timer is over');
+// для каждого нового вызова передавать класс таймера, конечную дату в формате '29 Jun 2025 17:28' и сообщение об окончании таймера 'The timer is over'
+// Передача конечной даты
+// initCountdown('.countdown', '27 Jun 2025 12:30', 'The timer is over');
+// Передача количества секунд (сутки = 86400с) (1 час = 3600секунд)
+// initCountdown('.countdown', 86440, 'The timer is over'); // Таймер на 1 день (86400 секунд)
 
 // addDate();
 
@@ -27,8 +25,10 @@ import { pageNavigation } from './modules/page-navigation';
 
 // tabs();
 
- headerFon();
+// headerFon();
 
- menuBurger();
+// menuBurger();
 
 // pageNavigation();
+
+// spollers();
---
+++ src/js/modules/spollers.js
@@ -0,0 +1,123 @@
+import { _slideDown, _slideUp, _slideToggle } from "./slideUp";
+/*
+data-one-spoiler функция акордеона, может быть открыт только один спойлер из всех
+data-spoilers пустой атрибут вешаем на родителя, если надо вывести спойлеры, которые будут работать на любом разрешении экрана
+"button" data-spoiler вешаем пустой атрибут на кнопку заголовок открывания
+data-spoilers="650,min" чтобы включался на экранах шире 650
+data-spoilers="800,max" чтобы включался на экранах меньше 650
+*/
+export function spollers() {
+	const spollersArray = document.querySelectorAll('[data-spollers]');
+	if (spollersArray.length > 0) {
+		// Получение обычных слойлеров
+		const spollersRegular = Array.from(spollersArray).filter(function (item, index, self) {
+			return !item.dataset.spollers.split(",")[0];
+		});
+		// Инициализация обычных слойлеров
+		if (spollersRegular.length > 0) {
+			initSpollers(spollersRegular);
+		}
+
+		// Получение слойлеров с медиа запросами
+		const spollersMedia = Array.from(spollersArray).filter(function (item, index, self) {
+			return item.dataset.spollers.split(",")[0];
+		});
+
+		// Инициализация слойлеров с медиа запросами
+		if (spollersMedia.length > 0) {
+			const breakpointsArray = [];
+			spollersMedia.forEach(item => {
+				const params = item.dataset.spollers;
+				const breakpoint = {};
+				const paramsArray = params.split(",");
+				breakpoint.value = paramsArray[0];
+				breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : "max";
+				breakpoint.item = item;
+				breakpointsArray.push(breakpoint);
+			});
+
+		// Получаем уникальные брейкпоинты
+		let mediaQueries = breakpointsArray.map(function (item) {
+			return '(' + item.type + "-width: " + item.value + "px)," + item.value + ',' + item.type;
+		});
+		mediaQueries = mediaQueries.filter(function (item, index, self) {
+			return self.indexOf(item) === index;
+		});
+
+		// Работаем с каждым брейкпоинтом
+		mediaQueries.forEach(breakpoint => {
+			const paramsArray = breakpoint.split(",");
+			const mediaBreakpoint = paramsArray[1];
+			const mediaType = paramsArray[2];
+			const matchMedia = window.matchMedia(paramsArray[0]);
+
+			// Объекты с нужными условиями
+			const spollersArray = breakpointsArray.filter(function (item) {
+				if (item.value === mediaBreakpoint && item.type === mediaType) {
+					return true;
+				}
+			});
+			// Событие
+			matchMedia.addListener(function () {
+				initSpollers(spollersArray, matchMedia);
+			});
+			initSpollers(spollersArray, matchMedia);
+		});
+	}
+	// Инициализация
+	function initSpollers(spollersArray, matchMedia = false) {
+		spollersArray.forEach(spollersBlock => {
+			spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
+			if (matchMedia.matches || !matchMedia) {
+				spollersBlock.classList.add('init');
+				initSpollerBody(spollersBlock);
+				spollersBlock.addEventListener("click", setSpollerAction);
+			} else {
+				spollersBlock.classList.remove('init');
+				initSpollerBody(spollersBlock, false);
+				spollersBlock.removeEventListener("click", setSpollerAction);
+			}
+		});
+	}
+	// Работа с контентом
+	function initSpollerBody(spollersBlock, hideSpollerBody = true) {
+		const spollerTitles = spollersBlock.querySelectorAll('[data-spoller]');
+		if (spollerTitles.length > 0) {
+			spollerTitles.forEach(spollerTitle => {
+				if (hideSpollerBody) {
+					spollerTitle.removeAttribute('tabindex');
+					if (!spollerTitle.classList.contains('active')) {
+						spollerTitle.nextElementSibling.hidden = true;
+					}
+				} else {
+					spollerTitle.setAttribute('tabindex', '-1');
+					spollerTitle.nextElementSibling.hidden = false;
+				}
+			});
+		}
+	}
+	function setSpollerAction(e) {
+		const el = e.target;
+		if (el.hasAttribute('data-spoller') || el.closest('[data-spoller]')) {
+			const spollerTitle = el.hasAttribute('data-spoller') ? el : el.closest('[data-spoller]');
+			const spollersBlock = spollerTitle.closest('[data-spollers]');
+			const oneSpoller = spollersBlock.hasAttribute('data-one-spoller') ? true : false;
+			if (!spollersBlock.querySelectorAll('.slide').length) {
+				if (oneSpoller && !spollerTitle.classList.contains('active')) {
+					hideSpollersBody(spollersBlock);
+				}
+				spollerTitle.classList.toggle('active');
+				_slideToggle(spollerTitle.nextElementSibling, 500);
+			}
+			e.preventDefault();
+		}
+	}
+	function hideSpollersBody(spollersBlock) {
+		const spollerActiveTitle = spollersBlock.querySelector('[data-spoller].active');
+		if (spollerActiveTitle) {
+			spollerActiveTitle.classList.remove('active');
+			_slideUp(spollerActiveTitle.nextElementSibling, 500);
+		}
+	}
+	}
+}
